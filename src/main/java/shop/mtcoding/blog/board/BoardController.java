d763427032f4c9b4a5058ccf4b177c5e65426670
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-03-23 22:06:06 +0900

    게시글 목록 보기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,25 +1,24 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import shop.mtcoding.blog.user.User;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
     private final HttpSession session;
+    private final BoardRepository boardRepository;
 
     @GetMapping({"/", "/board"})
-    public String index() {
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        if (sessionUser == null) {
-            System.out.println("로그인 안 된 상태입니다.");
-        } else {
-            System.out.println("로그인 된 상태입니다.");
-        }
+    public String index(HttpServletRequest request) {
+        List<Board> boardList = boardRepository.findAll();
+        request.setAttribute("boardList", boardList);
         return "index";
     }
 
